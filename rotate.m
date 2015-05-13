834f3b88554f00951ef15be525dd64d8ecde7a7a
Atom1c <romino@inbox.ru>
2015-05-13 18:41:48 +0600

    Find the longest line and rotate image to make it horizontal

--- rotate.m
+++ rotate.m
@@ -1,4 +1,4 @@
-filename = 'pro1.png'
+filename = 'nop.png'
 I = imread(filename);
 figure(1),
 imshow(I);
@@ -6,37 +6,32 @@ level=graythresh(I)
 BW=im2bw(I,level);
 BW = edge(BW,'canny');
 [H,theta,rho] = hough(BW);
-%figure(2),
-%imshow(H,[],'XData',theta,'YData',rho,...
-   %     'InitialMagnification','fit');
-   %xlabel('\theta'), ylabel('\rho');
-   %axis on, axis normal, hold on;
-    P = houghpeaks(H,5,'threshold',ceil(0.3*max(H(:))));
-    x = theta(P(:,2)); 
-    y = rho(P(:,1));
-    %plot(x,y,'s','color','white');
-    lines = houghlines(BW,theta,rho,P,'FillGap',5,'MinLength',7);
-    %figure, imshow(I), hold on
-    max_len = 0;
-    for k = 1:length(lines)
-	       xy = [lines(k).point1; lines(k).point2];
-	          %plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
-
-		     % Отображение начала и конца линий
-		       % plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
-		          %plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
-
-			     % Определение конечной точки
-			        len = norm(lines(k).point1 - lines(k).point2);
-				   if ( len > max_len)
-					         max_len = len;
-						       xy_long = xy;
-						          end
-						  end
-
-						  a=sqrt(abs((lines(1).point1(1)-lines(1).point2(1))^2 + (lines(1).point1(2)-lines(1).point2(2))^2))
-						  b=sqrt(abs((lines(1).point1(1)-lines(1).point2(1))^2 + (lines(1).point1(2)-lines(1).point1(2))^2))
-						  fi=real(acos(b/a))*180/pi
-						  i1=imrotate(I,-fi,'bilinear');
-						  figure(2),imshow(i1)
+ P = houghpeaks(H,5,'threshold',ceil(0.3*max(H(:))));
+ x = theta(P(:,2)); 
+ y = rho(P(:,1));
+ lines = houghlines(BW,theta,rho,P,'FillGap',5,'MinLength',7);
+ %figure, imshow(I), hold on
+ max_len = 0;
+ lenline=[]
+ for k = 1:length(lines)
+	     lenline=[lenline sqrt(abs((lines(k).point1(1)-lines(k).point2(1))^2 + (lines(k).point1(2)-lines(k).point2(2))^2))]
+	        xy = [lines(k).point1; lines(k).point2];
+		   % Определение конечной точки
+		      len = norm(lines(k).point1 - lines(k).point2);
+		         if ( len > max_len)
+				       max_len = len;
+				             xy_long = xy;
+					        end
+					end
+					[xmax,imax]=max(lenline)
+					lenline(imax)=0
+					[xmax1,imax1] =max(lenline)
+					a=sqrt(abs((lines(imax).point1(1)-lines(imax).point2(1))^2 + (lines(imax).point1(2)-lines(imax).point2(2))^2))
+					b=sqrt(abs((lines(imax).point1(1)-lines(imax).point2(1))^2 + (lines(imax).point1(2)-lines(imax).point1(2))^2))
+					fi=real(acos(b/a))*180/pi
+					a2 = sqrt(abs((lines(imax1).point1(1)-lines(imax1).point2(1))^2 + (lines(imax1).point1(2)-lines(imax1).point2(2))^2))
+					b2 = sqrt(abs((lines(imax1).point1(1)-lines(imax1).point2(1))^2 + (lines(imax1).point1(2)-lines(imax1).point1(2))^2))
+					i1=imrotate(I,-fi,'bilinear');
+					fi2 = real(acos(b2/a2))*180/pi
+					figure(2),imshow(i1)
 
